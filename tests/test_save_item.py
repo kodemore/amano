3137c8be439a687c032700e22cf8f582b96bee6d
Dawid Kraczkowski <dawid.kraczkowski@gmail.com>
2022-10-09 14:17:07 +0200

    Add support for saving updated item

--- tests/test_save_item.py
+++ tests/test_save_item.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from amano import Item, Table
 
 
-def test_can_save_item(default_dynamodb_client, default_table) -> None:
+def test_can_save_new_item(default_dynamodb_client, default_table) -> None:
     # given
     @dataclass
     class Track(Item):
@@ -20,3 +20,24 @@ def test_can_save_item(default_dynamodb_client, default_table) -> None:
     # then
     assert result
     assert my_table.get("Tool", "Reflection")
+
+
+def test_can_save_modified_item(default_dynamodb_client, default_table) -> None:
+    # given
+    @dataclass
+    class Track(Item):
+        artist_name: str
+        track_name: str
+        album_name: str
+
+    my_table = Table[Track](default_dynamodb_client, default_table)
+
+    # when
+    item = Track("Tool", "Reflection", "Lateralus")
+    assert my_table.put(item)
+
+    # then
+    item.album_name = "New Album"
+    assert my_table.save(item)
+    result = my_table.get("Tool", "Reflection")
+    assert result.album_name == "New Album"
